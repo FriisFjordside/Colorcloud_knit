e93a0bcbf5d27ae88a2bab7ccfa8a1fc2ad82e3f
Unknown <mattff1997@gmail.com>
2018-02-19 16:23:33 +0100

    adding NTH js

--- colorcloud.js
+++ colorcloud.js
@@ -1,31 +1,27 @@
+document.getElementById("tekst2").onclick = function () {
+    foldUd()
+};
 
-
-/*
-var finalbanner = document.getElementById("finalbanner");
-
-
-var bil8 = document.getElementById("billedeboks8");
-
-function xhover(){
-    
-    console.log("jeg kommer fra hover()");
-    
-    document.getElementById("billedeboks8").innerHTML = "YOU";
-    
-}
-
-
-xhover();
-*/
-
-document.getElementById("finalbanner_activate").onmouseover = function() {myFunction()};
-
-// her er den funktion, der startes:
-function myFunction() {
+function foldUd() {
     console.log("hurra!");
 
     document
-		.getElementById("finalbanner")
-        .innerHTML = "ART <br> IMITATES <br> YOU";
-     
-}
+        .getElementById("tekst2")
+        .innerHTML = "<p>Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLUDSTUDIO_KNIT. Hans kreativitet og innovative tilgang er fundamentet i virksomheden. Derudover er Tommy en initiativrig person, som altid spekulerer i sine fremtidige projekter.</p>";
+
+    /*------------------------------------------------------------------------- */
+
+ 
+        /*------------------------------------------------------------------------- */
+
+    
+    document.getElementById("finalbanner_activate").onmouseover = function () {
+        myFunction()
+    };
+
+    function myFunction() {
+        console.log("hurra!");
+
+        document
+            .getElementById("finalbanner")
+            .innerHTML = "ART <br> IMITATES <br> YOU";}
