50088068722ac315b2d83e19e00223058b7b2cf0
Unknown <mattff1997@gmail.com>
2018-02-20 09:56:59 +0100

    4. slide complete

--- colorcloud.js
+++ colorcloud.js
@@ -13,9 +13,17 @@ document.getElementById("tekst3").onclick = function () {
     fuldUd()
     document
     .getElementById("tekst3")
-    .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Germed blev hans favorit sweater starten på et nyt eventyr."
+    .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Germed blev hans favorit sweater starten på et nyt eventyr.";
 }
 /*--------------------------------------------------------------------------------------*/
+document.getElementById("tekst4").onclick = function () {
+    fuldUd()
+    document
+    .getElementById("tekst4")
+    .innerHTML = "Den græske filosof, Aristoteles sagde en engang, 'Art imitates nature.' Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simole skønheder. <br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppes former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knuttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.";
+}
+/*--------------------------------------------------------------------------------------*/
+
     document.getElementById("finalbanner_activate").onmouseover = function () {
         myFunction()
     };
