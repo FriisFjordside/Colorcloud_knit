835e02a477f8143b4d59cee2cfaf62d771194f04
Unknown <mattff1997@gmail.com>
2018-02-20 10:03:31 +0100

    5.slide complete

--- colorcloud.js
+++ colorcloud.js
@@ -23,6 +23,13 @@ document.getElementById("tekst4").onclick = function () {
     .innerHTML = "Den græske filosof, Aristoteles sagde en engang, 'Art imitates nature.' Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simole skønheder. <br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppes former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knuttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.";
 }
 /*--------------------------------------------------------------------------------------*/
+document.getElementById("tekst6").onclick = function () {
+    fuldUd()
+    document
+    .getElementById("tekst6")
+    .innerHTML = "Det er ikke kun kvaliteten af sweateren, der har stor betydning for Tommy. Han mener, at den vigtigste del af processen er, at inddrage kunden. Forud for kreationen af sweateren, har kunden en dialog med Tommy. Dialogen er begyndelsen på den selvindsigt, der skaber hver enkelt strik. Dialogen danner ikke kun ramme for det fysiske udtryk af strikken, men skaber også den emotionelle association til trøjen."
+}
+/*--------------------------------------------------------------------------------------*/
 
     document.getElementById("finalbanner_activate").onmouseover = function () {
         myFunction()
