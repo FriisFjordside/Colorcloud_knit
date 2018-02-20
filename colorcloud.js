4bf6f81dc1c5f2a06081792b0f3496853d73f9f0
Unknown <mattff1997@gmail.com>
2018-02-20 12:27:01 +0100

    Added JS Dropdown arrows

--- colorcloud.js
+++ colorcloud.js
@@ -1,44 +1,56 @@
-document.getElementById("tekst2").onclick = function () {
-    fuldUd()
-}; 
-function fuldUd() {
+document.getElementById("readmore1").onclick = function () {
+    foldUd()
+} 
+function foldUd() {
     console.log("yeds");
     
     document
     .getElementById("tekst2")
     .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Han kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. <br> Brian Sørensen praktisere jura i form af implementering af foretningsstradegier og foretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
+
 }
-/*--------------------------------------------------------------------------------------*/
-document.getElementById("tekst3").onclick = function () {
-    fuldUd()
+   
+/*---------------------------------------------------------------------------------*/
+document.getElementById("readmore2").onclick = function () {
+    foldUd
+
+    
     document
     .getElementById("tekst3")
     .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Germed blev hans favorit sweater starten på et nyt eventyr.";
 }
+
 /*--------------------------------------------------------------------------------------*/
-document.getElementById("tekst4").onclick = function () {
-    fuldUd()
+document.getElementById("readmore3").onclick = function () {
+    foldUd
+
+ 
     document
     .getElementById("tekst4")
     .innerHTML = "Den græske filosof, Aristoteles sagde en engang, 'Art imitates nature.' Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simole skønheder. <br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppes former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knuttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.";
 }
 /*--------------------------------------------------------------------------------------*/
-document.getElementById("tekst6").onclick = function () {
-    fuldUd()
+document.getElementById("readmore4").onclick = function () {
+    foldUd
+
+ 
     document
     .getElementById("tekst6")
     .innerHTML = "Det er ikke kun kvaliteten af sweateren, der har stor betydning for Tommy. Han mener, at den vigtigste del af processen er, at inddrage kunden. Forud for kreationen af sweateren, har kunden en dialog med Tommy. Dialogen er begyndelsen på den selvindsigt, der skaber hver enkelt strik. Dialogen danner ikke kun ramme for det fysiske udtryk af strikken, men skaber også den emotionelle association til trøjen.";
 }
 /*--------------------------------------------------------------------------------------*/
-document.getElementById("tekst6ekstra").onclick = function () {
-    fuldUd()
+document.getElementById("readmore5").onclick = function () {
+    foldUd
+
+    
     document
     .getElementById("tekst6ekstra")
     .innerHTML = "Det er betydningsfuldt for Tommy, at have et stort udvalg af forskellige farver tråd. Dette er, som Tommy understreger, fordi at tråden repræsenterer temperaturen på mennesket. Mulighederne for de mange farvekombinationer af tråde, og variationer i faconen af sweateren, skaber uendelige muligheder. På den måde bliver hver kundes strik personliggjort, og den følelsesmæssige forbindelse bliver forstærket.";
 }
 /*--------------------------------------------------------------------------------------*/
-document.getElementById("tekst7").onclick = function () {
-    fuldUd()
+document.getElementById("readmore6").onclick = function () {
+    foldUd
+
     document
     .getElementById("tekst7")
     .innerHTML = "Kødbyen er en gammel bydel af Aarhus, hvor mange kreative virksomheder holder til. Området er stille og uforstyrret med undtagelsen for havets brusen, og et par enkelte sæler der titter frem fra overfladen. Studiets kreativitet skinner tydeligt igennem i form af arbejdsmiljøet. Strikkemaskiner, tråd i adskillige farver og designskitser pryder lokalet og udgør COLORCLOUDSTUDIO_KNITs essens.";
