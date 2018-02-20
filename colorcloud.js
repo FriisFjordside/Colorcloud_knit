57ca55bd1a439045283ab18957fc0768b2aaa852
Unknown <mattff1997@gmail.com>
2018-02-20 09:32:38 +0100

    2. slide complete

--- colorcloud.js
+++ colorcloud.js
@@ -1,20 +1,14 @@
 document.getElementById("tekst2").onclick = function () {
-    foldUd()
-};
-
-function foldUd() {
-    console.log("hurra!");
-
-    document
-        .getElementById("tekst2")
-        .innerHTML = "<p>Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLUDSTUDIO_KNIT. Hans kreativitet og innovative tilgang er fundamentet i virksomheden. Derudover er Tommy en initiativrig person, som altid spekulerer i sine fremtidige projekter.</p>";
-
-    /*------------------------------------------------------------------------- */
-
- 
-        /*------------------------------------------------------------------------- */
-
+    fuldUd()
+}; 
+function fuldUd() {
+    console.log("yeds");
     
+    document
+    .getElementById("tekst2")
+    .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Han kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. Brian Sørensen praktisere jura i form af implementering af foretningsstradegier og foretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
+}
+/*--------------------------------------------------------------------------------------*/
     document.getElementById("finalbanner_activate").onmouseover = function () {
         myFunction()
     };
@@ -25,3 +19,4 @@ function foldUd() {
         document
             .getElementById("finalbanner")
             .innerHTML = "ART <br> IMITATES <br> YOU";}
+/*-----------------------------------------------------------------------------------*/
