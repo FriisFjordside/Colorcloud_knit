eeb4a71945b277594e963fe84ad974ed1fd50a9a
Unknown <mattff1997@gmail.com>
2018-02-20 09:40:40 +0100

    3. slide complete

--- colorcloud.js
+++ colorcloud.js
@@ -6,7 +6,14 @@ function fuldUd() {
     
     document
     .getElementById("tekst2")
-    .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Han kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. Brian Sørensen praktisere jura i form af implementering af foretningsstradegier og foretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
+    .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Han kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. <br> Brian Sørensen praktisere jura i form af implementering af foretningsstradegier og foretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
+}
+/*--------------------------------------------------------------------------------------*/
+document.getElementById("tekst3").onclick = function () {
+    fuldUd()
+    document
+    .getElementById("tekst3")
+    .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Germed blev hans favorit sweater starten på et nyt eventyr."
 }
 /*--------------------------------------------------------------------------------------*/
     document.getElementById("finalbanner_activate").onmouseover = function () {
