1234b962ccf6063006de52c02e30da0e058a1c8c
Unknown <mattff1997@gmail.com>
2018-02-20 15:51:09 +0100

    Færdig arbejde

--- colorcloud.js
+++ colorcloud.js
@@ -6,7 +6,7 @@ function foldUd() {
     
     document
     .getElementById("tekst2")
-    .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Hans kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. <br> Brian Sørensen praktisere jura i form af implementering af forretningsstrategier og forretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
+    .innerHTML = "Tommy Bruun har en lang baggrund som selvstændig inden for arkitekturfaget, som tydeligt genskabes hos COLORCLOUDSTUDIO_KNIT. Hans kreativitet og innovative tilgang er fundamentet i virksomheden derudover er Tommy en initiativrig person, som altid spekulerer i sin fremtidige projekter. <br><br> Brian Sørensen praktisere jura i form af implementering af forretningsstrategier og forretningsoptimering. Brian varetager mere praktiske opgaver i henhold til COLORCLOUDSTUDIO_KNIT.";
 
 }
    
@@ -17,7 +17,7 @@ document.getElementById("readmore2").onclick = function () {
     
     document
     .getElementById("tekst3")
-    .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Dermed blev hans favorit sweater starten på et nyt eventyr.";
+    .innerHTML = "Fascinationen af strik og kashmir startede allerede tilbage i Berlin, 1994. På en gårtur i byens gader, faldt Tommy over en forretning som lavede strik i netop den slags uld. Ud af de mange muligheder udså Tommy sig en hvid og lyseblå sweater med grovere vævning. Han prøvede trøjen på, og følte med det samme hvordan kashmiren omfavnede hans figur. Tommy var solgt på stedet, og købte sweateren. <br><br> Sweateren bev hurtigt hans ynglings trøje, og blev flittigt brugt gennem årene. Grundet hans nysgerrige sind, blev kashmiren med tiden en forundring, og denne forundring til betagelse. Dermed blev hans favorit sweater starten på et nyt eventyr.";
 }
 
 /*--------------------------------------------------------------------------------------*/
@@ -27,7 +27,7 @@ document.getElementById("readmore3").onclick = function () {
  
     document
     .getElementById("tekst4")
-    .innerHTML = 'Den græske filosof, Aristoteles sagde en engang, <i>"Art imitates nature."</i> Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simple skønheder. <br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppens former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knyttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.';
+    .innerHTML = 'Den græske filosof, Aristoteles sagde en engang, <i>"Art imitates nature."</i> Netop dette inspirerede Tommy, og ændrede hans perspektiv på skønhed. Denne åbenbaring banede vej for en dybere værdsættelse af selv naturens simple skønheder. <br><br> Kashmir er et naturmateriale som er let, blødt og smidigt. Derfor tilpasser ulden sig med tiden kroppens former. Som Tommy beskriver, er filosofien bag COLORCLOUDSTUDIO_KNIT tæt knyttet til kashmir, fordi de ønsker at skabe en strik der tilpasser sig hvert menneskes form. Han ønsker, at genskabe den følelse han fik tilbage i 1994 i hans strik. Derfor er sweateren ikke konstrueret af et tilfældigt materiale, men netop kashmir fordi ulden med tiden bliver en genskabelse af vedkommendes skikkelse. Pasformen som strikken med tiden får, tilføjer en stærk personlig følelse til trøjen.';
 }
 /*--------------------------------------------------------------------------------------*/
 document.getElementById("readmore4").onclick = function () {
@@ -66,5 +66,5 @@ document.getElementById("readmore6").onclick = function () {
 
         document
             .getElementById("finalbanner")
-            .innerHTML = "ART <br> IMITATES <br> YOU";}
+            .innerHTML = "ART IMITATES YOU";}
 /*-----------------------------------------------------------------------------------*/
